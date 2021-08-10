521e34e99d5e983374c41d517126ec2da6118804
ywliu722 <yaowenliu.cs07@nctu.edu.tw>
2021-08-10 16:25:10 +0800

    add some indexes and calculate the theoretical bitrate

--- publisher.py
+++ publisher.py
@@ -2,27 +2,40 @@ from threading import current_thread
 from time import sleep
 import paho.mqtt.client as mqtt
 
-serviceIP = "192.168.1.15"
+serviceIP = "192.168.1.148"
 servicePort = 1883
 topic = "TESTING"
 
 mqttc = mqtt.Client("python_pub")
 mqttc.connect(serviceIP, servicePort)
+current_nss = ""
 current_mcs = ""
 current_ppdu = ""
+current_GI = True
 
 while True:
 	try:
 		input_file = open('/sys/kernel/debug/ieee80211/phy0/netdev:wlan0/stations/08:c5:e1:f1:fc:11/stats','r')
+		#input_file = open('/sys/kernel/debug/ieee80211/phy0/netdev:wlan0/stations/48:45:20:98:d4:1a/stats','r')
 		input = input_file.readlines()
 		input_file.close()
+		nss = input[0].split()[5]
 		mcs_index = input[0].split()[6]
+		if len(input[0].split()) == 8:
+			GI = True
+		else:
+			GI = False
 		ppdu_rate = input[1].split()[2].split('%')[0]
-		if mcs_index != current_mcs or ppdu_rate != current_ppdu:
-			output = mcs_index + " " + ppdu_rate
+		if nss != current_nss or mcs_index != current_mcs or ppdu_rate != current_ppdu or GI != current_GI:
+			if GI:
+				output = nss + " " + mcs_index + " " + ppdu_rate + " SGI"
+			else:
+				output = nss + " " + mcs_index + " " + ppdu_rate + " GI"
 			mqttc.publish(topic, output)
+			current_nss = nss
 			current_mcs = mcs_index
 			current_ppdu = ppdu_rate
+			current_GI = GI
 			print(output)
 	except:
 		continue
--- subscriber.py
+++ subscriber.py
@@ -1,18 +1,32 @@
 import json
 import paho.mqtt.client as mqtt
 
+bitrate= {"GI": {}, "SGI": {}}
+bitrate["GI"] = {"MCS0": 29.3, "MCS1": 58.5, "MCS2": 87.8, "MCS3": 117.0, "MCS4": 175.5,
+                         "MCS5": 234.0,"MCS6": 263.3, "MCS7": 292.5, "MCS8": 351.0, "MCS9": 390.0}
+bitrate["SGI"] = {"MCS0": 32.5, "MCS1": 65.0, "MCS2": 97.5, "MCS3": 130.0, "MCS4": 195.0,
+                         "MCS5": 260.0,"MCS6": 292.5, "MCS7": 325.0, "MCS8": 390.0, "MCS9": 433.3}
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     client.subscribe("TESTING")
 
 def on_message(client, userdata, msg):
     input = msg.payload.decode().split()
-    mcs_index = input[0]
-    ppdu_rate = float(input[1])
+    nss = float(input[0][3])
+    mcs_index = input[1]
+    ppdu_rate = float(input[2])
+    GI = input[3]
+    theoretical_rate = bitrate[GI][mcs_index] * (100 - ppdu_rate) * 0.01 * nss
     output = {
+        "NSS" : nss,
         "MCS" : mcs_index,
-        "PPDU" : ppdu_rate
+        "PPDU" : ppdu_rate,
+        "GI" : GI
     }
+    print(output)
+    print(theoretical_rate)
+    '''
     try:
         output_file = open("output.json","w")
         json.dump(output, output_file)
@@ -20,9 +34,10 @@ def on_message(client, userdata, msg):
         print(output)
     except:
         pass
+    '''
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("192.168.1.15", 1883, 60)
+client.connect("192.168.1.148", 1883, 60)
 client.loop_forever()
