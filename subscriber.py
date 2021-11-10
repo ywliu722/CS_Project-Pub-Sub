d49cfc6a930057cb49d544bdb2230cc761b96693
ywliu722 <yaowenliu.cs07@nctu.edu.tw>
2021-11-10 23:57:45 +0800

    add throughput table and add some features

--- subscriber.py
+++ subscriber.py
@@ -8,7 +8,23 @@ bitrate["GI"] = {"MCS0": 29.3, "MCS1": 58.5, "MCS2": 87.8, "MCS3": 117.0, "MCS4"
 bitrate["SGI"] = {"MCS0": 32.5, "MCS1": 65.0, "MCS2": 97.5, "MCS3": 130.0, "MCS4": 195.0,
                          "MCS5": 260.0,"MCS6": 292.5, "MCS7": 325.0, "MCS8": 390.0, "MCS9": 433.3}
 '''
+
+# throughput of each rate from experiment
+throughput = {"NSS1": {"GI": {}, "SGI": {}}, 
+              "NSS2": {"GI": {}, "SGI": {}}}
+throughput["NSS1"]["GI"] = {"MCS0": 29.3, "MCS1": 58.5, "MCS2": 87.8, "MCS3": 117.0, "MCS4": 175.5,
+                            "MCS5": 234.0,"MCS6": 263.3, "MCS7": 292.5, "MCS8": 351.0, "MCS9": 390.0}
+throughput["NSS1"]["SGI"] = {"MCS0": 24.45, "MCS1": 49.45, "MCS2": 74.0, "MCS3": 96.05, "MCS4": 141.5,
+                             "MCS5": 185.5,"MCS6": 216.0, "MCS7": 240.5, "MCS8": 297.0, "MCS9": 317.5}
+
+throughput["NSS2"]["GI"] = {"MCS0": 29.3, "MCS1": 58.5, "MCS2": 87.8, "MCS3": 117.0, "MCS4": 175.5,
+                            "MCS5": 234.0,"MCS6": 263.3, "MCS7": 292.5, "MCS8": 351.0, "MCS9": 390.0}
+throughput["NSS2"]["SGI"] = {"MCS0": 49.65, "MCS1": 93.7, "MCS2": 139.5, "MCS3": 192.5, "MCS4": 280.0,
+                             "MCS5": 383.0,"MCS6": 409.0, "MCS7": 452.0, "MCS8": 517.0, "MCS9": 571.0}
+ 
 second_device = False
+bw_1080 = 49    # required bandwidth for 1080p
+bw_720 = 25     # required bandwidth for 720p
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -16,7 +32,7 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     input = msg.payload.decode().split()
-    nss = float(input[0][3])
+    nss = input[0]
     mcs_index = input[1]
     GI = input[2]
     data_len = float(input[3])
@@ -31,6 +47,17 @@ def on_message(client, userdata, msg):
         "GI" : GI,
         "Data Length": data_len
     }
+    max_throughput = throughput[nss][GI][mcs_index]
+    airtime_percentage = tx / (interval * 1000000)
+    expected_throughput = max_throughput * airtime_percentage
+    if expected_throughput > bw_1080:
+        video_quality = "1080p"
+    else:
+        video_quality = "720p"
+    output = {
+        "quality" : video_quality
+    }
+    '''
     #print(output)
     print("Current Rate: " + str((data_len * 8 / 1000000)/interval) + " Mbits/s")
     if(second_device):
@@ -45,13 +72,18 @@ def on_message(client, userdata, msg):
     print("-----------------------------------------------")
     '''
     try:
-        output_file = open("output.json","w")
-        json.dump(output, output_file)
-        output_file.close()
-        print(output)
+        input_file = open ('output.json','r')
+        json_array = json.load(input_file)
+        input_file.close()
+        current_quality = json_array['quality']
+        if current_quality != video_quality:
+            output_file = open("output.json","w")
+            json.dump(output, output_file)
+            output_file.close()
+            print(output)
     except:
         pass
-    '''
+    
 
 client = mqtt.Client()
 client.on_connect = on_connect
